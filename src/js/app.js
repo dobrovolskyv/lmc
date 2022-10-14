ed4404a8651a595a713c586b109f74cce328e078
Vlad Dobrovolsky <vladislavdobrovolskij@MacBook-Air-Vladislav-2.local>
2022-10-14 23:49:19 +0400

    main page

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,36 @@ import * as flsFunctions from "./modules/functions.js";
 
 flsFunctions.isWebp();
 
-import Swiper, { Navigation, Pagination } from "swiper";
+// import Swiper, { Navigation, Pagination } from "swiper";
 
-const swiper = new Swiper();
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    speed: 400,
+    slidesPerView: 2,
+   direction: 'vertical',
+   loop: true,
+ 
+   // If we need pagination
+   pagination: {
+     el: '.swiper-pagination',
+   },
+ 
+   // Navigation arrows
+   navigation: {
+     nextEl: '.swiper-button-next'
+   },
+   autoplay:{
+       delay: 2000,
+       stopOnLastSlides: true,
+       disableOnInteraction: false
+   },
+//    effect: 'fade',
+ //   cubeEffect:{
+ //       slideShadows: true,
+ //       shadow: true,
+ //       shadowOffset: 20,
+ //       shadowScale: 0.94
+ //   }
+   
+   });
+
