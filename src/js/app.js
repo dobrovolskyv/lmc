42bc820e7ed44488d9f4c5c8c7896d439de78661
Vlad Dobrovolsky <vladislavdobrovolskij@MacBook-Air-Vladislav-2.local>
2022-10-16 01:57:04 +0400

    main page

--- src/js/app.js
+++ src/js/app.js
@@ -4,34 +4,83 @@ flsFunctions.isWebp();
 
 // import Swiper, { Navigation, Pagination } from "swiper";
 
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    speed: 400,
-    slidesPerView: 2,
-   direction: 'vertical',
-   loop: true,
- 
-   // If we need pagination
-   pagination: {
-     el: '.swiper-pagination',
-   },
- 
-   // Navigation arrows
-   navigation: {
-     nextEl: '.swiper-button-next'
-   },
-   autoplay:{
-       delay: 2000,
-       stopOnLastSlides: true,
-       disableOnInteraction: false
-   },
-//    effect: 'fade',
- //   cubeEffect:{
- //       slideShadows: true,
- //       shadow: true,
- //       shadowOffset: 20,
- //       shadowScale: 0.94
- //   }
-   
-   });
+const swiper = new Swiper('.specialist__sliders__swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 3.2,
+  spaceBetween: 40,
 
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+  autoplay: {
+    delay: 2500,
+  },
+});
+
+
+const clinicSwiper = new Swiper('.clinic__right-swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  
+  autoplay: {
+    delay: 2200,
+  },
+});
+
+
+const reviewsSwiper = new Swiper('.reviews__sliders__swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 2,
+  spaceBetween: 20,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  
+  autoplay: {
+    delay: 2500,
+  },
+});
