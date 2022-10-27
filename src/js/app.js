194cdf7e0bbcba7eb186bbce73a6ab4210acfeb3
Vlad Dobrovolsky <vladislavdobrovolskij@MacBook-Air-Vladislav-2.local>
2022-10-27 17:00:00 +0400

    pages

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,7 @@ import * as flsFunctions from "./modules/functions.js";
 flsFunctions.isWebp();
 
 // import Swiper, { Navigation, Pagination } from "swiper";
-
+document.addEventListener('DOMContentLoaded', (e)=>{
 const headerSlides = new Swiper('.header__slides__swiper', {
   // Optional parameters
   direction: 'horizontal',
@@ -122,8 +122,11 @@ articlesBtn.addEventListener('click', (e)=>{
     articlesCard.style.display = "none";
   }
 })
+
+
 const offerCard = document.querySelector('.offer__bot-hidden');
 const offerBtn = document.querySelector('.offer__btn');
+
 
 offerBtn.addEventListener('click', (e)=>{
   if (offerCard.style.display === "none") {
@@ -132,8 +135,25 @@ offerBtn.addEventListener('click', (e)=>{
     offerCard.style.display = "none";
   }
 })
+const offerServicesCard = document.querySelector('.offer-services__bot-hidden');
+const offerServicesBtn = document.querySelector('.offer-services__btn');
+
+offerServicesBtn.addEventListener('click', (e)=>{
+  if (offerServicesCard.style.display === "none") {
+    offerServicesCard.style.display = "grid";
+  } else {
+    offerServicesCard.style.display = "none";
+  }
+})
 
 
-// document.addEventListener('DOMContentLoaded', (e)=>{
+const questions = document.querySelectorAll('.about-doctor__contentbox');
+const answer = document.querySelectorAll('.about-doctor__answer')
 
-// })
+questions.forEach((question)=>{
+  question.addEventListener('click', ()=>{
+    question.classList.toggle('active')
+  })
+})
+
+})
